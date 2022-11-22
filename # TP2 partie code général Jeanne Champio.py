79e8ae7234dd6deb0012807ecf407129cc04c3be
jeanne Champion <jeanne.champion@cpe.fr>
2022-11-22 10:03:49 +0100

    g

--- # TP2 partie code général Jeanne Champio.py
+++ # TP2 partie code général Jeanne Champio.py
@@ -1,6 +1,8 @@
 # TP2 partie code général Jeanne Champion et Thibaud Martinez le 22/11/2022
 
 
+dictionnaire ={"le" : 0, "la" : 0, "chat" : 2, "souris" : 2, "martin" : 4,
+"mange" : 3, "la" : 0, "petite" : 1, "joli" : 1, "grosse" : 1,
+"bleu" : 1, "verte" : 1, "dort" : 3,"julie" : 4, "jean" : 4, "." : 5}
 
 
-Entrée_Act=['I','O','N','v','b']
